04a5130acdabd6d468ea441af194e2ae939b2685
patururamesh70 <ramesh@42works.net>
2024-11-22 16:27:30 +0530

    implement uploadoimages

--- ios1/ViewController.swift
+++ ios1/ViewController.swift
@@ -8,12 +8,12 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    let uiVC = UploadimageViewController()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+  
 
 }
 
